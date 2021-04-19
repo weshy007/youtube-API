592cc7b9b202597ef6412610954506211499e658
weshy007 <josephwaweru96@gmail.com>
2021-04-19 11:33:06 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from googleapiclient.discovery import build
+
+
+
+youtube = build('youtube', 'v3', developerKey=api_key )
+
+request = youtube.channels().list(
+    part='statistics', 
+    forUsername='schafer5'
+)
+
+response = request.execute()
+
+print(response)
